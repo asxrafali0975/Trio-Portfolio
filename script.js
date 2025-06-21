3bf801d486f8225c9140742b95bea81a92141846
AshrafAli0975 <2023bca113@axiscolleges.in>
2025-06-21 20:13:48 +0530

    script.js

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
 
-// let tl = gsap.timeline();
+let tl = gsap.timeline();
 
 
-// tl.from("#page1",{
-//    scale:0,
-//    delay:0.7,
-//    duration:1,
-//    opacity:0
+tl.from("#page1",{
+   scale:0,
+   delay:0.7,
+   duration:1,
+   opacity:0
 
-// })
-
-
-
-// tl.from("nav",{
-//    scale:0,
-//    delay:0.5,
-//    duration:1,
-//    opacity:0,
-//    y:-300,
-
-// })
-
-
-// tl.from(["#left"," #right","#textcontent"],{
-//    scale:0,
-//    delay:0.5,
-//    duration:1.4,
-//    opacity:0,
-
-// })
+})
 
 
 
-// tl.from(["#imagediv","#name"],{
-//      scale:0,
-//   //  delay:1.2,
-//    duration:1.4,
-//    opacity:0
+tl.from("nav",{
+   scale:0,
+   delay:0.5,
+   duration:1,
+   opacity:0,
+   y:-300,
+
+})
 
 
-// })
+tl.from(["#left"," #right","#textcontent"],{
+   scale:0,
+   delay:0.5,
+   duration:1.4,
+   opacity:0,
 
-// tl.to("#imagediv",{
-//   x:15,
-//   delay:1,
-//   yoyo:true,
-//   repeat:Infinity,
-
-
-// })
+})
 
 
-// tl.to("#imagediv",{
-//   y:-15,
-//   yoyo:true,
-//     repeat:Infinity,
+
+tl.from(["#imagediv","#name"],{
+     scale:0,
+  //  delay:1.2,
+   duration:1.4,
+   opacity:0
+
+
+})
+
+tl.to("#imagediv",{
+  x:15,
+  delay:1,
+  yoyo:true,
+  repeat:Infinity,
+
+
+})
+
+
+tl.to("#imagediv",{
+  y:-15,
+  yoyo:true,
+    repeat:Infinity,
   
 
 
-// })
+})
 
 
-// tl.to("#imagedv",{
-//   x:-15,
-//   yoyo:true,
-//     repeat:Infinity,
+tl.to("#imagedv",{
+  x:-15,
+  yoyo:true,
+    repeat:Infinity,
   
 
 
-// })
+})
 
-// tl.to("#imagediv",{
-//   y:15,
-//   yoyo:true,
-//     repeat:Infinity,
+tl.to("#imagediv",{
+  y:15,
+  yoyo:true,
+    repeat:Infinity,
   
 
 
-// })
+})
 
 
 const options = {
